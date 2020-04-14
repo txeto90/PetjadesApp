ac4e5b482c6beb99cbe88c0998e6cf20f2500b05
Jomoniz <herpessenyor@gmail.com>
2020-04-14 13:01:01 +0200

    V7 14/04/2020 Sols falta:  - incorporar imatges caca/rastre.  - incorporar imatges petjades.  - buscador

--- app/src/main/java/com/example/petjadesapp/AboutActivity.java
+++ app/src/main/java/com/example/petjadesapp/AboutActivity.java
@@ -1,5 +1,6 @@
 package com.example.petjadesapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,15 +9,19 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
     private TextView txtPhoto;
-    private String author = "Jakob Fahr \nCesar Pollo \nAloma Alcina \nFrank Sengpiel \nTomeu Bosch \n Benoit Nabholz \nJeff Skrentny \nJupreudhomme \n" +
-            "Andy Bridges \nBill Palmer \nRoberto Ghiglia \nPaul Cools \nAlejandro Garrido \nLauren Glevanik \nAnnika Lindqvist \n" +
-            "Setbusch \nRonald Werson \nPeter Trimming \nSamuel Faure \nMax Hofmann \nDee Doubleu \nSkyrk";
+    private TextView txtMaps;
+    private TextView txtCopyRight;
+    private String author = "Jakob Fahr, \tCesar Pollo, \tAloma Alcina, \tFrank Sengpiel, \tTomeu Bosch, \tBenoit Nabholz, \tJeff Skrentny, \tJupreudhomme, \t" +
+            "Andy Bridges, \tBill Palmer, \tRoberto Ghiglia, \tPaul Cools, \tAlejandro Garrido, \tLauren Glevanik, \tAnnika Lindqvist, \t" +
+            "Setbusch, \tRonald Werson, \tPeter Trimming, \tSamuel Faure, \tMax Hofmann, \tDee Doubleu, \tSkyrk, \tMagriet B., \tGonzalo Mucientes, \t" +
+            "Carlos Bocos, \tJose Cazador, \tThierry, \tClaudia Komesu, \tJarvo, \tMarco Vicariotto.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +29,12 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        txtMaps = findViewById(R.id.txtMaps);
+        txtCopyRight = findViewById(R.id.txtCopyRight);
         txtPhoto = findViewById(R.id.txtPhotos);
         txtPhoto.setText(author);
-
+        txtMaps.setMovementMethod(LinkMovementMethod.getInstance());
+        txtCopyRight.setMovementMethod(LinkMovementMethod.getInstance());
 
 
 
