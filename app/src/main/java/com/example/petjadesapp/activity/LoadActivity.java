06897922e4cda92c376d7957837bb05ecd774ace
Jomoniz <herpessenyor@gmail.com>
2020-04-27 15:19:20 +0200

    V8 27/04/2020 Sols falta:  - multiidioma  - base dades desde fitxer

---
+++ app/src/main/java/com/example/petjadesapp/activity/LoadActivity.java
@@ -0,0 +1,33 @@
+package com.example.petjadesapp.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import com.example.petjadesapp.R;
+
+public class LoadActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load);
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(2000);  //Delay of 2 seconds
+                } catch (Exception e) {
+
+                } finally {
+                    Intent i = new Intent(LoadActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+
+}
